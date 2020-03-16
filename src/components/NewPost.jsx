df35c9cd39578964f007e219e20fef77ec1aeb5a
Pawel <ssopel10@gmail.com>
2020-03-16 23:54:10 +0100

    a few fixes

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -7,53 +7,53 @@ import H2 from '../styles/shared/h2';
 import Form from '../styles/NewPost';
 
 const NewPost = () => {
-    const [textAreaValue, setTextAreaValue] = useState('');
-    const currentUser = useContext(UserContext);
-    const history = useHistory();
+  const [textAreaValue, setTextAreaValue] = useState('');
+  const currentUser = useContext(UserContext);
+  const history = useHistory();
 
-    const addPost = async e => {
-        e.preventDefault();
+  const addPost = async e => {
+    e.preventDefault();
 
-        let userData;
-        await firebase
-            .firestore()
-            .collection('users')
-            .doc(currentUser.uid)
-            .get()
-            .then(doc => {
-                userData = doc.data();
-            })
-            .then(() => {
-                firebase
-                    .firestore()
-                    .collection('posts')
-                    .add({
-                        userId: currentUser.uid,
-                        userName: userData.fullName,
-                        content: textAreaValue,
-                        createdAt: Date.now(),
-                        likes: [],
-                        comments: []
-                    });
-            })
-            .then(() => setTextAreaValue(''))
-            .then(() => history.push('/home'));
-    };
+    let userData;
+    await firebase
+      .firestore()
+      .collection('users')
+      .doc(currentUser.uid)
+      .get()
+      .then(doc => {
+        userData = doc.data();
+      })
+      .then(() => {
+        firebase
+          .firestore()
+          .collection('posts')
+          .add({
+            userId: currentUser.uid,
+            userName: userData.fullName,
+            content: textAreaValue,
+            createdAt: Date.now(),
+            likes: [],
+            comments: []
+          });
+      })
+      .then(() => setTextAreaValue(''))
+      .then(() => history.push(`${process.env.PUBLIC_URL}/home`));
+  };
 
-    return (
-        <Section>
-            <H2>Create post</H2>
-            <Form onSubmit={addPost}>
-                <Form.Textarea
-                    placeholder="Write something..."
-                    rows="18"
-                    value={textAreaValue}
-                    onChange={e => setTextAreaValue(e.currentTarget.value)}
-                ></Form.Textarea>
-                <Form.Button type="submit">Post</Form.Button>
-            </Form>
-        </Section>
-    );
+  return (
+    <Section>
+      <H2>Create post</H2>
+      <Form onSubmit={addPost}>
+        <Form.Textarea
+          placeholder="Write something..."
+          rows="18"
+          value={textAreaValue}
+          onChange={e => setTextAreaValue(e.currentTarget.value)}
+        ></Form.Textarea>
+        <Form.Button type="submit">Post</Form.Button>
+      </Form>
+    </Section>
+  );
 };
 
 export default NewPost;
