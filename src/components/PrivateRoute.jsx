4821495300939607932ee3f38804c986e7ac54c6
Pawel <ssopel10@gmail.com>
2020-03-16 22:58:58 +0100

    change redirect route in privateRoute component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ children, auth, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-            render={() =>
-                auth === true ? children : <Redirect to={'/login'} />
-            }
-        />
-    );
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        auth === true ? (
+          children
+        ) : (
+          <Redirect to={`${process.env.PUBLIC_URL}/login`} />
+        )
+      }
+    />
+  );
 };
 
 export default PrivateRoute;
